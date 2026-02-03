71b860bd112d82ad5da2d8d83a47f04d1d790af9
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-02-03 10:09:40 +0100

    Accessing properties from index cshtml cs file

--- RazorBoatApp2026/Pages/Index.cshtml.cs
+++ RazorBoatApp2026/Pages/Index.cshtml.cs
@@ -7,9 +7,12 @@ namespace RazorBoatApp2026.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public string Name { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Name = "Peter";
         }
 
         public void OnGet()
